24988831d540303912a87a5e123be98dbf811bd8
Irina Motuz <irina@irinas-mbp.mynetworksettings.com>
2026-03-26 12:55:16 -0400

    Complete About and Footer section

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,9 +2,9 @@ function renderNav() {
   return `
     <nav class="navbar">
       <ul class="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#blog">Blog</a></li>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="index.html#about">About</a></li>
+        <li><a href="blog.html">Blog</a></li>
       </ul>
     </nav>
   `;
